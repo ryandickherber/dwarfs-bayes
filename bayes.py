aa476c475f36ed397b6c9d1e28245c6c0b76b5ea
Ryan Dickherber <ryandickherber@gmail.com>
2012-07-17 16:39:39 -0500

    bayes.py works with code refactoring. check math!

--- bayes.py
+++ bayes.py
@@ -5,10 +5,6 @@ import math
 from numpy import r_
 import scipy.interpolate
 
-Mchi=120 #GeV #TODO: fill in realistic value for mass
-Slist=[0,1,2,3,4,5,6,7,8,9,10]#TODO: fill in realistic values for cross section
-observations=[]
-
 class Observation:
 	def __init__(self):
 		#TODO: fill in realistic default values
@@ -22,7 +18,6 @@ class Observation:
 		self.Ei=[50.0, 150.0, 250.0] #true energy average in GeV
 		self.Eirange=[(10.0,100.0),(100.0,200.0),(200.0,400.0)]
 		self.P_Ei=[0.25, 0.5, 0.25]
-
 		#avg effective areas in each range, meters squared
 		self.Ai=[\
 			[5000.0],\
@@ -39,105 +34,99 @@ class Observation:
 			[1.0],\
 			[1.0],\
 			]
+		#J factors, measured in whatever units they are usually measured in
 		self.Ji=[5, 6, 7]
 		self.Jirange=[(4.5,5.5),(5.5,6.5),(6.5,7.5)]
 		self.P_Ji=[0.25, 0.5, 0.25]
 
-	
+class Bayes:
+	def __init__(self, Mchi=400.0, Slist=[0,1,2,3,4,5,6,7,8,9,10]):
+		self.Mchi=Mchi #GeV #TODO: fill in realistic value for mass
 
-def integrate_dNdE(Ei0,Ei1):
+		#TODO: fill in realistic values for cross section
+		self.Slist=Slist
+
+		self.observations=[]
+		self.P_S_sum=0
+		self.memoizedict={}
+
+	def P_Nnpi(self, obs, Nnpi):
+		prob=PoissonApprox2(obs.Nmi,obs.Ci*Nnpi)
+		#print("P_Nnpi: %s" % prob)
+		return prob
+
+	def P_Npi_JiAiEiNnpiS(self, obs, iJi, iAi, iEi, Nnpi, S):
+		Mchi=self.Mchi
+		Ngi=S/(8*math.pi*(Mchi**2))*\
+			integrate_dNdE(obs.Eirange[iEi][0],obs.Eirange[iEi][1])\
+			*obs.Ai[iEi][iAi]*obs.Ti*obs.Ji[iJi]
+		lmbda=Nnpi+Ngi
+		k=obs.Npi
+		prob=PoissonApprox2(k,lmbda)
+		#print("P_Npi_JiAiEiNnpiS: %s" % prob)
+		return PoissonApprox2(k,lmbda)
+
+	def P_Npi_NnpiS(self, obs, Nnpi, S):
+		p=[]
+		for iEi, Ei in enumerate(obs.Ei):
+			for iAi, Ai in enumerate(obs.Ai[iEi]):
+				for iJi, Ji in enumerate(obs.Ji):
+					P=self.P_Npi_JiAiEiNnpiS(obs, iJi, iAi, iEi, Nnpi, S)
+					P=P*obs.P_Ji[iJi]*obs.P_Ai[iEi][iAi]*obs.P_Ei[iEi]
+					p.append(P)
+		prob=math.fsum(p)
+		#print("P_Npi_NnpiS: %s" % prob)
+		return math.fsum(p)
+
+	def P_Npi_S(self, obs, S):
+		Nnpi_cutoff=int(3*obs.Nmi/obs.Ci)
+		if Nnpi_cutoff<30:
+			Nnpi_cutoff=30
+		p=[self.P_Nnpi(obs, Nnpi)*self.P_Npi_NnpiS(obs, Nnpi, S)\
+			for Nnpi in range(Nnpi_cutoff)]
+		prob=math.fsum(p)
+		#print("P_Npi_S: %s" % prob)
+		return prob
+
+	def P_S_Np_product(self, S):
+		Mchi=self.Mchi
+		T=[]
+		for obs in self.observations:
+			P=self.P_Npi_S(obs, S)
+			if P!=0:
+				T.append(math.log(P))
+		t=math.fsum(T)
+		t=math.exp(t)
+		return t
+
+	def P_S_Np_product_memoized(self, S):
+		Mchi=self.Mchi
+		if S in self.memoizedict:
+			if Mchi in self.memoizedict[S]:
+				return self.memoizedict[S][Mchi]
+			else:
+				self.memoizedict[S][Mchi]=self.P_S_Np_product(S)
+				return self.memoizedict[S][Mchi]
+		self.memoizedict[S]={}
+		self.memoizedict[S][Mchi]=self.P_S_Np_product(S)
+		return self.memoizedict[S][Mchi]
+
+	def P_S_Np(self, S):
+		Mchi=self.Mchi
+		top=self.P_S_Np_product_memoized(S)
+		bottom=0
+		B=[]
+		if self.P_S_sum==0:
+			for S in self.Slist:
+				t=self.P_S_Np_product_memoized(S)
+				B.append(t)
+			self.P_S_sum=math.fsum(B)
+		bottom=self.P_S_sum
+		return top/bottom
+
+def integrate_dNdE(Ei0, Ei1):
 	#TODO: make this work
 	return 1.0
-
-def P_Nnpi(obs, Nnpi):
-	prob=PoissonApprox2(obs.Nmi,obs.Ci*Nnpi)
-	#print("P_Nnpi: %s" % prob)
-	return prob
-
-def P_Npi_JiAiEiNnpiS(obs, Ji, Ai, Ei0, Ei1, Nnpi, S, Mchi):
-	Ti=obs.Ti
-	Ngi=S/(8*math.pi*(Mchi**2))*\
-		integrate_dNdE(Ei0,Ei1)*Ai*Ti*Ji
-	lmbda=Nnpi+Ngi
-	k=obs.Npi
-	prob=PoissonApprox2(k,lmbda)
-	#print("P_Npi_JiAiEiNnpiS: %s" % prob)
-	return PoissonApprox2(k,lmbda)
-
-def P_Npi_NnpiS(obs, Nnpi, S, Mchi):
-	p=[]
-	for iEi, Ei in enumerate(obs.Ei):
-		for iAi, Ai in enumerate(obs.Ai[iEi]):
-			for iJi, Ji in enumerate(obs.Ji):
-				Ei0=obs.Eirange[iEi][0]
-				Ei1=obs.Eirange[iEi][1]
-				P=P_Npi_JiAiEiNnpiS(obs, Ji, Ai, Ei0, Ei1, Nnpi, S, Mchi)
-				P=P*obs.P_Ji[iJi]*obs.P_Ai[iEi][iAi]*obs.P_Ei[iEi]
-				p.append(P)
-	prob=math.fsum(p)
-	#print("P_Npi_NnpiS: %s" % prob)
-	return math.fsum(p)
-
-def P_Npi_S(obs, S, Mchi):
-	Nnpi_cutoff=int(3*obs.Nmi/obs.Ci)
-	if Nnpi_cutoff<30:
-		Nnpi_cutoff=30
-	p=[P_Nnpi(obs, Nnpi)*P_Npi_NnpiS(obs, Nnpi, S, Mchi)\
-		for Nnpi in range(Nnpi_cutoff)]
-	prob=math.fsum(p)
-	#print("P_Npi_S: %s" % prob)
-	return prob
-
-#memoizedict={}
-#def P_S_Np_memoized(S, Mchi):
-#	if S in memoizedict:
-#		if Mchi in memoizedict[S]:
-#			return memoizedict[S][Mchi]
-#		P=P_S_Np(S, Mchi)
-#		memoizedict[S][Mchi]=P
-#		return P
-#	P=P_S_Np(S, Mchi)
-#	memoizedict[S]={}
-#	memoizedict[S][Mchi]=P
-#	return P
-
-def P_S_Np_product(S, Mchi):
-	T=[]
-	for obs in observations:
-		P=P_Npi_S(obs, S, Mchi)
-		if P!=0:
-			T.append(math.log(P))
-	t=math.fsum(T)
-	t=math.exp(t)
-	return t
-
-memoizedict={}
-def P_S_Np_product_memoized(S, Mchi):
-	global memoizedict
-	if S in memoizedict:
-		if Mchi in memoizedict[S]:
-			return memoizedict[S][Mchi]
-		else:
-			memoizedict[S][Mchi]=P_S_Np_product(S, Mchi)
-			return memoizedict[S][Mchi]
-	memoizedict[S]={}
-	memoizedict[S][Mchi]=P_S_Np_product(S, Mchi)
-	return memoizedict[S][Mchi]
-
-P_S_sum=0
-def P_S_Np(S, Mchi):
-	global P_S_sum
-
-	top=P_S_Np_product_memoized(S, Mchi)
-	bottom=0
-	B=[]
-	if P_S_sum==0:
-		for S in Slist:
-			t=P_S_Np_product_memoized(S, Mchi)
-			B.append(t)
-		P_S_sum=math.fsum(B)
-	bottom=P_S_sum
-	return top/bottom
 
 def Poisson(k,lmbda):
 	if (lmbda==0):
@@ -155,6 +144,7 @@ def PoissonApprox1(k,lmbda):
 
 def PoissonApprox2(k,lmbda):
 	#higher order stirling approximation... good enough
+	#this is the function that should be used for a Poisson
 	if (lmbda==0):
 		return 0
 	if (k==0):
@@ -248,10 +238,10 @@ class PoissonInterp:
 		return self.f(k)
 
 if __name__=="__main__":
-	Mchi=130 #GeV
-	S=Slist[3] #??? units?
-	observations=[Observation()]
-	P=P_S_Np(S, Mchi)
-	print("P: %s" % P)
+	b=Bayes()
+	b.observations=[Observation()]
+	for S in b.Slist:
+		P=b.P_S_Np(S)
+		print("S, P: %s, %s" % (S,P))
 	print("Sum over P: %s" % math.fsum(\
-		[P_S_Np(S, Mchi) for S in Slist]))
+		[b.P_S_Np(S) for S in b.Slist]))
